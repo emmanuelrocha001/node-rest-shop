d35bebe1f561ee9b4b95d232e1cd7884daf8ed34
emmanuelrocha001 <emmanuelrocha001@gmail.com>
2019-08-19 15:30:56 -0500

    basic routing implemented

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
 // sets up middleware
-app.use((req, res, next) => {
-  res.status(200).json({
-    message: 'It works!'
-  });
-});
+// urls with /products will be forwarded
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
 
 module.exports = app;
